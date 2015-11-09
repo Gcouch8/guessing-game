6ad281e3fb0056f8a43c590bb7f12801bb21e9db
Daniel Zwelling <dzwellingmusic@gmail.com>
2015-11-09 10:38:42 -0800

    Fix object constructor

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-var GuessingGame = function() {
+var GuessingGame = function(question, answer, results) {
     this.question = question;
     this.answer = answer;
-    this.correct = correct;
-    this.wrong = wrong;
-    this.image = image;
+    this.correct = results.correct;
+    this.wrong = results.wrong;
+    this.image = results.image;
     this.renderCorrect = function() {
         var s = document.createElement('img').setAttribute('src', this.image);
         var p = document.createElement('p');
@@ -15,23 +15,20 @@ var GuessingGame = function() {
     };
     this.runGame = function() {
         var x = prompt(this.question);
-
-        if (this.answer == 'yes') {
-            if (this.answer == x){
-                this.renderCorrect();
-            } else {
-                this.renderWrong();
-            }
-        } else if (typeof(this.answer) == 'number') {
-            if (x < this.answer) {
-                y = crea
-            }
-        }
+    //     if (this.answer == 'yes') {
+    //         if (this.answer == x){
+    //             this.renderCorrect();
+    //         } else {
+    //             this.renderWrong();
+    //         }
+    //     } else if (typeof(this.answer) == 'number') {
+    //         if (x < this.answer) {
+    //             y = crea
+    //         }
+    //     }
     }
 };
 
-var question1 = new GuessingGame(questions[0], answers[0], correct[0], wrong[0] images[0]);
+var question1 = new GuessingGame(questions[0], answers[0], {correct: correct[0], wrong: wrong[0], image: images[0]});
 
-
-
-question1.runGame()
+// question1.runGame();
