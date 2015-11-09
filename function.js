d5c39790991f6944b518c5b2ba05aaa07c4a521d
Daniel Zwelling <dzwellingmusic@gmail.com>
2015-11-09 13:56:09 -0800

    Handle number responses

--- function.js
+++ function.js
@@ -1,4 +1,5 @@
 var finalScore = 0;
+
 var questions = ['Did I grow up in Minnesota?',
                  'Was I a member of the US Olympic Curling Team?',
                  'Have I been volcano boarding?',
@@ -6,29 +7,31 @@ var questions = ['Did I grow up in Minnesota?',
                  'Do I have two dogs?',
                  'How many cats do I have?'
                 ];
-
 var answers = ['yes', 'no', 'yes', 25, 'yes', 2]
-var correct = ["That's correct, have you been following me?",
-                           "That's correct, sadly I didn't make team.",
-                           "That's correct, and the volcano is active!",
-                           "That's correct, a quarter of a century. How time flies.",
-                           "That's correct, two big dogs, Boomer and Jax!",
-                           "That's correct, Baggins and Chirp!"
-                          ];
 
-var wrong= ["That's incorrect. Oh ya you betcha I grew up in Minnesota",
-                             "That's incorrect. Maybe they would've won gold that year with me.",
-                             "That's incorrect. It's like you don't even know me.",
-                             "That's incorrect. Do you really think I'm that old?",
-                             "That's incorrect. I'm flattered, but I'm not that young.",
-                             "That's incorrect. Couldn't live without my best buds.",
-                             "That's incorrect. I'm not a crazy cat person, at least I hope not.",
-                             "That's incorrect. I've got more than that."
-                            ];
+var correct = ["That's correct, have you been following me?",
+               "That's correct, sadly I didn't make team.",
+               "That's correct, and the volcano is active!",
+               "That's correct, a quarter of a century. How time flies.",
+               "That's correct, two big dogs, Boomer and Jax!",
+               "That's correct, Baggins and Chirp!"
+              ];
+
+var wrong = ["That's incorrect. Oh ya you betcha I grew up in Minnesota",
+            "That's incorrect. Maybe they would've won gold that year with me.",
+            "That's incorrect. It's like you don't even know me.",
+            "That's incorrect. Couldn't live without my best buds.",
+           ];
+var wrongNumber1 = [ "That's incorrect. Do you really think I'm that old?",
+                     "That's incorrect. I'm flattered, but I'm not that young.",]
+
+var wrongNumber2 = [ "That's incorrect. I'm not a crazy cat person, at least I hope not.",
+                     "That's incorrect. I've got more than that."]
+
 var images =['http://www.findlocalbanks.com/images/states/minnesota.gif',
             'http://i.huffpost.com/gen/1621418/images/n-JEREMY-ABBOTT-large570.jpg',
             'http://www.gsu.edu/wp-content/uploads/2013/11/Volcano-erupting-600x300.jpg',
             'http://images.mentalfloss.com/sites/default/files/styles/article_640x430/public/happy-25th.jpg',
             'http://labsarefamily.com/wp-content/uploads/2015/07/lab5.jpg',
             'http://images.mentalfloss.com/sites/default/files/styles/article_640x430/public/700salvador_dali_nywts.jpg'
-]
+            ]
--- test.js
+++ test.js
@@ -1,14 +1,12 @@
 var button = document.getElementById('start');
 
-
-
 start.addEventListener('click', function(event){
     question1.runGame(document.getElementById('aOne'), document.getElementById('imageOne'));
     question2.runGame(document.getElementById('aTwo'), document.getElementById('imageTwo'));
     question3.runGame(document.getElementById('aThree'), document.getElementById('imageThree'));
-    // question4.runGame(document.getElementById('aFour'), document.getElementById('imageFour'));
-    // question5.runGame(document.getElementById('aFive'), document.getElementById('imageFive'));
-    // question6.runGame(document.getElementById('aSix'), document.getElementById('imageSix'));
+    question4.runGame(document.getElementById('aFour'), document.getElementById('imageFour'));
+    question5.runGame(document.getElementById('aFive'), document.getElementById('imageFive'));
+    question6.runGame(document.getElementById('aSix'), document.getElementById('imageSix'));
 });
 
 
@@ -17,7 +15,7 @@ var GuessingGame = function(question, answer, results) {
     this.question = question;
     this.answer = answer;
     this.correct = results.correct;
-    this.wrong = results.wrong;
+    this.wrong = results.wrong || results.wrongNumberLow || results.wrongNumberHigh;
     this.image = results.image;
     this.runGame = function(element1, element2) {
         var response = prompt(this.question);
@@ -25,8 +23,20 @@ var GuessingGame = function(question, answer, results) {
             if (response == this.answer) {
                element1.innerHTML = this.correct;
                element2.src = this.image;
+               console.log('image should change here');
             } else {
                 element1.innerHTML = this.wrong;
+            }
+        } else {
+            if (response < this.answer) {
+                element1.innerHTML = results.wrongNumberLow;
+            }
+            if (response > this.answer) {
+                element1.innerHTML = results.wrongNumberHigh;
+            }
+            if (response == this.answer) {
+                element1.innerHTML = this.correct;
+                element2.src = this.image;
             }
         }
     }
@@ -35,9 +45,9 @@ var GuessingGame = function(question, answer, results) {
 var question1 = new GuessingGame(questions[0], answers[0], {correct: correct[0], wrong: wrong[0], image: images[0]});
 var question2 = new GuessingGame(questions[1], answers[1], {correct: correct[1], wrong: wrong[1], image: images[1]});
 var question3 = new GuessingGame(questions[2], answers[2], {correct: correct[2], wrong: wrong[2], image: images[2]});
-var question4 = new GuessingGame(questions[3], answers[3], {correct: correct[3], wrong: wrong[3], image: images[3]});
-var question5 = new GuessingGame(questions[4], answers[4], {correct: correct[4], wrong: wrong[4], image: images[4]});
-var question6 = new GuessingGame(questions[5], answers[5], {correct: correct[5], wrong: wrong[5], image: images[5]});
+var question4 = new GuessingGame(questions[3], answers[3], {correct: correct[3], wrongNumberHigh: wrongNumber1[0], wrongNumberLow: wrongNumber1[1], image: images[3]});
+var question5 = new GuessingGame(questions[4], answers[4], {correct: correct[4], wrong: wrong[3], image: images[4]});
+var question6 = new GuessingGame(questions[5], answers[5], {correct: correct[5], wrongNumberHigh: wrongNumber2[0], wrongNumberLow: wrongNumber2[1], image: images[5]});
 
 
 
