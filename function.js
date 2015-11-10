945432ffbf6d1371a5a77ecb6c53b65653a943c7
GARETH COUCH <gcouch8@gmail.com>
2015-11-09 19:23:32 -0800

    add function and eventlisterner

--- function.js
+++ function.js
@@ -7,38 +7,34 @@ var questions = ['Did I grow up in Minnesota?',
                  'How many cats do I have?'
                 ];
 
-var answers = ['yes', 'no', 'yes', 25, 'yes', 2]
+var answers = ['yes', 'no', 'yes', 25, 'yes', 2];
 var correct = ["That's correct, have you been following me?",
-                           "That's correct, sadly I didn't make team.",
-                           "That's correct, and the volcano is active!",
-                           "That's correct, a quarter of a century. How time flies.",
-                           "That's correct, two big dogs, Boomer and Jax!",
-                           "That's correct, Baggins and Chirp!"
-                          ];
+               "That's correct, sadly I didn't make team.",
+               "That's correct, and the volcano is active!",
+               "That's correct, a quarter of a century. How time flies.",
+               "That's correct, two big dogs, Boomer and Jax!",
+               "That's correct, Baggins and Chirp!"
+                ];
 
-var wrong= ["That's incorrect. Oh ya you betcha I grew up in Minnesota",
-                             "That's incorrect. Maybe they would've won gold that year with me.",
-                             "That's incorrect. It's like you don't even know me.",
-                             "That's incorrect. Do you really think I'm that old?",
-                             "That's incorrect. I'm flattered, but I'm not that young.",
-                             "That's incorrect. Couldn't live without my best buds.",
-                             "That's incorrect. I'm not a crazy cat person, at least I hope not.",
-                             "That's incorrect. I've got more than that."
-                            ];
+var wrong = ["That's incorrect. Oh ya you betcha I grew up in Minnesota",
+              "That's incorrect. Maybe they would've won gold that year with me.",
+              "That's incorrect. It's like you don't even know me.",
+              "That's incorrect. I'm flattered, but I'm not that young.",
+              "That's incorrect. Couldn't live without my best buds.",
+              "That's incorrect. I've got more than that."
+              ];
+
+var wrong2= ["That's incorrect. Do you really think I'm that old?",
+              "That's incorrect. I'm not a crazy cat person, at least I hope not.",
+            ];
 var images =['http://www.findlocalbanks.com/images/states/minnesota.gif',
             'http://i.huffpost.com/gen/1621418/images/n-JEREMY-ABBOTT-large570.jpg',
             'http://www.gsu.edu/wp-content/uploads/2013/11/Volcano-erupting-600x300.jpg',
             'http://images.mentalfloss.com/sites/default/files/styles/article_640x430/public/happy-25th.jpg',
             'http://labsarefamily.com/wp-content/uploads/2015/07/lab5.jpg',
             'http://images.mentalfloss.com/sites/default/files/styles/article_640x430/public/700salvador_dali_nywts.jpg'
-]
+            ];
 
-
-var button = document.getElementById('start');
-
-start.addEventListener('click', function(event){
-  question1.runGame();
-});
 
 // //var answers = ['yes', 'no', 'yes', 25, 'yes', 2];
 // //var userAnswer = prompt(questions[0]);
@@ -61,6 +57,8 @@ start.addEventListener('click', function(event){
 
 
 // })();
+
+
 
 
 
--- test.js
+++ test.js
@@ -1,37 +1,73 @@
+var button = document.getElementById('start');
+
+start.addEventListener('click', function(event){
+  question1.runGame();
+  question2.runGame();
+  question3.runGame();
+  question4.runGame();
+  question5.runGame();
+  question6.runGame();
+});
+
 var GuessingGame = function() {
     this.question = question;
     this.answer = answer;
     this.correct = correct;
     this.wrong = wrong;
+    this.wrong2= wrong2;
     this.image = image;
     this.renderCorrect = function() {
         var s = document.createElement('img').setAttribute('src', this.image);
         var p = document.createElement('p');
         p.innerHTML = this. correct;
+        finalScore++;
     };
     this.renderWrong = function() {
         var s = document.createElement('p');
         s.innerHTML = this.wrong;
     };
+    this.renderWrong2 = function(){
+        var l = document.createElement('p');
+        l.innerHTML=this.wrong2;
+    }
     this.runGame = function() {
         var x = prompt(this.question);
 
         if (this.answer == 'yes') {
             if (this.answer == x){
                 this.renderCorrect();
-            } else {
+            }
+            else {
                 this.renderWrong();
             }
         } else if (typeof(this.answer) == 'number') {
             if (x < this.answer) {
-                y = crea
+               this.renderWrong();
             }
+            else if (x === this.answer){
+                this.renderCorrect();
+
+            }
+            else{
+               this.renderWrong2();
+            }
+
+
         }
     }
 };
 
-var question1 = new GuessingGame(questions[0], answers[0], correct[0], wrong[0] images[0]);
 
+var question1 = new GuessingGame(questions[0], answers[0], correct[0], wrong[0], images[0]);
+var question2 = new GuessingGame(questions[1], answers[1], correct[1], wrong[1], images[1]);
+var question3 = new GuessingGame(questions[2], answers[2], correct[2], wrong[2], images[2]);
+var question4 = new GuessingGame(questions[3], answers[3], correct[3], wrong[3], wrong2[0], images[3]);
+var question5 = new GuessingGame(questions[4], answers[4], correct[4], wrong[4], images[4]);
+var question6 = new GuessingGame(questions[5], answers[5], correct[5], wrong[5], wrong2[1], images[5]);
 
-
-question1.runGame()
+question1.runGame();
+question2.runGame();
+question3.runGame();
+question4.runGame();
+question5.runGame();
+question6.runGame();
